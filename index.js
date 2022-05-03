f7399c91695b292c930fcb548d52c62c8f10a929
Himanshi Satpute <57482731+himanshi748@users.noreply.github.com>
2022-05-03 15:20:56 +0530

    day1

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root =ReactDOM.createRoot(document.getElementsByClassName('root')[0]);
+root.render(
+  <div>
+  <h1>My First React App 🤩</h1>
+  <p>lqwerty qwerty qwerty qwerty qwerty qwerty ...</p>
+  </div>
+  );
+
